f3e164e14c70533b711ef6270ddb41b5236f88c1
Jen Simmons <jen@jensimmons.com>
2013-04-09 17:06:31 -0400

    finishing up Animation 2.

--- assets/js/two.js
+++ assets/js/two.js
@@ -3,17 +3,15 @@
 
     // Flip the cards over
     // --------------------------------------------------------------------
-    // This adds a 'flip' class to things marked with 'hover' when someone is hovering.
+    // This adds a 'flip' class to things marked with 'hover' when someone is hovering, or touches.
     // This javascript was snagged from http://css3playground.com/flip-card.php
     // Thanks Chris Ruppel, http://chrisruppel.com
 
-    $('.hover').hover(function(){
+    $('.card').hover(function(){
       $(this).addClass('flip');
     },function(){
       $(this).removeClass('flip');
     });
 
-    //show
-    $('div.view-people ul').show();
   });
 })(jQuery);
