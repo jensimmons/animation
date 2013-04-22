f4c4dc5917612d9fa6120fedb6ddbaf976e4f82c
Jen Simmons <jen@jensimmons.com>
2013-04-22 17:27:03 -0400

    moremore

--- assets/js/three.js
+++ assets/js/three.js
@@ -1,1 +1,8 @@
-
+(function ($) {
+  $(document).ready(function() {
+    $('.fliplink').on('click', function (evt) {
+      evt.preventDefault();
+      $(this).parents('article:first').addClass('flip');
+    });
+  });
+})(jQuery);
