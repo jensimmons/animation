d3f6a9355fb58a05c241dab401454b444f1e3a53
Jen Simmons <jen@jensimmons.com>
2013-04-23 16:37:26 -0400

    a bit more

--- assets/js/three.js
+++ assets/js/three.js
@@ -1,8 +1,30 @@
 (function ($) {
   $(document).ready(function() {
+  
     $('.fliplink').on('click', function (evt) {
       evt.preventDefault();
       $(this).parents('article:first').addClass('flip');
     });
+
+    $('.fliplinkone').on('click', function (evt) {
+      evt.preventDefault();
+      $(this).parents('body').addClass('flipone');
+    });
+
+    $('.fliplinktwo').on('click', function (evt) {
+      evt.preventDefault();
+      $(this).parents('body').removeClass('flipone').addClass('fliptwo');
+    });
+
+    $('.fliplinkthree').on('click', function (evt) {
+      evt.preventDefault();
+      $(this).parents('body').removeClass('fliptwo').addClass('flipthree');
+    });
+
+    $('.fliplinkfour').on('click', function (evt) {
+      evt.preventDefault();
+      $(this).parents('body').removeClass('flipthree').addClass('flipfour');
+    });
+    
   });
 })(jQuery);
